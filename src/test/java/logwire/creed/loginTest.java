ef2e6dee630616eeacf7c2eb3079a12338b451f0
ELALAMIADAM <adamelalami27@gmail.com>
2026-09-07 15:03:01 +0200

    Add login tests for valid and invalid user credentials

---
+++ src/test/java/logwire/creed/loginTest.java
@@ -0,0 +1,33 @@
+package logwire.creed;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loginTest {
+    WebDriver driver;
+    @Test 
+    public void loginValid(){
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+    }
+    @Test 
+    public void loginInvalid(){
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user_fake");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String var = driver.findElement(By.cssSelector("[data-test=\"error\"]")).getText();
+        assertTrue(driver.findElement(By.cssSelector("[data-test=\"error\"]")).isDisplayed());
+        assertEquals("Epic sadface: Username and password do not match any user in this service", var);
+    }
+}
