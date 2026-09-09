0b4dbf930a20d2204d41970cea2b535ccd3bdaa8
ELALAMIADAM <adamelalami27@gmail.com>
2026-09-09 13:57:10 +0200

    Remove unused FirefoxDriver import from loginpfTest.java

--- src/test/java/faa/com/loginpfTest.java
+++ src/test/java/faa/com/loginpfTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import faa.com.PF.loginpf;
 
